b6cdd708bad0ae5e90a6ae589743e9c322f0f39b
izabela.cdsa <izabela.cdsa@gmail.com>
2019-12-08 11:04:50 -0300

    add splash

---
+++ app/src/main/java/com/choco/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.choco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        showLogin()
+    }
+
+    private fun showLogin() {
+        Handler().postDelayed({
+//            Actions.openLogin(this@SplashActivity)
+            finish()
+        }, 2000)
+    }
+}
