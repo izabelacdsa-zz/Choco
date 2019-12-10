82258e3f6cb2e4a16f9f3d0c063f710a7ccf8eee
izabela.cdsa <izabela.cdsa@gmail.com>
2019-12-09 22:56:33 -0300

    add module order list

---
+++ features/order/src/main/java/com/order/orderlist/activity/OrderListActivity.kt
@@ -0,0 +1,13 @@
+package com.order.orderlist.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.order.R
+
+class OrderListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_list)
+    }
+}
