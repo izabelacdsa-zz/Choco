82bff28f9501cf580754154976c527ae97aa266d
izabela.cdsa <izabela.cdsa@gmail.com>
2019-12-11 10:19:44 -0300

    adjust view

--- features/order/src/main/java/com/order/ordercheckout/ui/activity/OrderCheckoutActivity.kt
+++ features/order/src/main/java/com/order/ordercheckout/ui/activity/OrderCheckoutActivity.kt
@@ -3,11 +3,21 @@ package com.order.ordercheckout.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.order.R
+import kotlinx.android.synthetic.main.activity_order_checkout.*
 
 class OrderCheckoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_checkout)
+        initComponents()
     }
+
+    private fun initComponents() {
+        btnOrderProduct.setOnClickListener {
+
+        }
+    }
+
+
 }
