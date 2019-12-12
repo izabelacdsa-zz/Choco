6c5626a90359f97cf1c46d22bc42556e2488936d
izabela.cdsa <izabela.cdsa@gmail.com>
2019-12-12 09:09:08 -0300

    create confirmation screen

--- features/order/src/main/java/com/order/ordercheckout/ui/activity/ConfirmationOrderActivity.kt
+++ features/order/src/main/java/com/order/ordercheckout/ui/activity/ConfirmationOrderActivity.kt
@@ -2,12 +2,21 @@ package com.order.ordercheckout.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.actions.Actions
 import com.order.R
+import kotlinx.android.synthetic.main.activity_confirmation_order.*
 
 class ConfirmationOrderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmation_order)
+        initComponents()
+    }
+
+    private fun initComponents() {
+        btnLeaveApp.setOnClickListener {
+            Actions.openLoginFromOrderList(this@ConfirmationOrderActivity)
+        }
     }
 }
